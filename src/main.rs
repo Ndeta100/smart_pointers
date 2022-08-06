0384e74159b90a95af9ceaa3645c9934406b617c
Ndeta Innocent <you@example.com>
2022-08-06 19:23:30 +0300

    Using Cons and Nil

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,17 @@ fn main() {
     let s = vec!["Germany", "Estonia"];
     let b = Box::new(s);
     println!("b={:?}", b);
+    let list = List::Cons(
+        3,
+        Box::new(List::Cons(
+            10,
+            Box::new(List::Cons(12, Box::new(List::Nil))),
+        )),
+    );
+    println!("{:?}", list);
+}
+#[derive(Debug)]
+pub enum List {
+    Cons(i32, Box<List>),
+    Nil,
 }
