7e415864dac1993b12f1da299143ce2f7b50ea7c
Ndeta Innocent <you@example.com>
2022-08-07 03:12:31 +0300

    using RefCell

--- src/lib.rs
+++ src/lib.rs
@@ -78,3 +78,20 @@ pub fn ref_cell() {
     println!("b after ={:?}", b);
     println!("c after ={:?}", c);
 }
+//Weak references
+#[derive(Debug)]
+struct Node {
+    value: i32,
+    children: RefCell<Vec<Rc<Node>>>,
+}
+fn main() {
+    let leaf = Rc::new(Node {
+        value: 3,
+        children: RefCell::new(vec![]),
+    });
+    let branch = Rc::new(Node {
+        value: 5,
+        children: RefCell::new(vec![Rc::clone(&leaf)]),
+    });
+    ref_cell();
+}
